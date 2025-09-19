19800273fed0816b95bf7694600ccc5ffb8557b7
Kristina Solovyova <kristina.solovyova@weka.io>
2025-09-19 22:29:41 +0300

    feat: add key-value support to SetupOTelSDK and remove SchemaURL
    - Add variadic keysAndValues parameter to SetupOTelSDK for custom resource attributes
    - Remove semconv.SchemaURL to avoid community-reported merge conflicts
    - Improve error handling in newResource function
    - Pass context through resource creation chain

--- instrumentation/otel.go
+++ instrumentation/otel.go
@@ -9,11 +9,12 @@ import (
 
 	"github.com/go-logr/logr"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc/credentials"
 )
@@ -26,11 +27,13 @@ var (
 
 // SetupOTelSDK bootstraps the OpenTelemetry pipeline.
 // If it does not return an error, make sure to call shutdown for proper cleanup.
-func SetupOTelSDK(ctx context.Context, serviceName, serviceVersion string, logger logr.Logger) (shutdown func(context.Context) error, err error) {
+// Additional resource attributes can be provided as key-value pairs.
+func SetupOTelSDK(ctx context.Context, serviceName, serviceVersion string, logger logr.Logger, keysAndValues ...any) (shutdown func(context.Context) error, err error) {
 	logger.V(1).WithCallDepth(1).Info("Setting up OTel SDK", "service", serviceName, "version", serviceVersion)
 	Tracer = otel.Tracer(serviceName)
 
 	if otlpEndpoint == "" {
+		logger.V(2).WithCallDepth(1).Info("No OTLP endpoint configured - traces will not be exported")
 		return func(ctx context.Context) error {
 			return nil
 		}, nil
@@ -46,7 +49,7 @@ func SetupOTelSDK(ctx context.Context, serviceName, serviceVersion string, logge
 	otel.SetTextMapPropagator(prop)
 
 	// Set up trace provider.
-	tracerProvider, err := newTraceProvider(ctx, serviceName, serviceVersion, logger)
+	tracerProvider, err := newTraceProvider(ctx, serviceName, serviceVersion, logger, keysAndValues...)
 	if err != nil {
 		handleErr(err)
 		return shutdown, err
@@ -62,16 +65,34 @@ func SetupOTelSDK(ctx context.Context, serviceName, serviceVersion string, logge
 	}, err
 }
 
-func newResource(serviceName, serviceVersion string) *resource.Resource {
-	r, _ := resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(serviceName),
-			semconv.ServiceVersionKey.String(serviceVersion),
-		),
+func newResource(ctx context.Context, serviceName, serviceVersion string, keysAndValues ...any) (*resource.Resource, error) {
+	// Start with the basic required attributes
+	attrs := []attribute.KeyValue{
+		semconv.ServiceNameKey.String(serviceName),
+		semconv.ServiceVersionKey.String(serviceVersion),
+	}
+
+	// Add any additional resource attributes provided using the existing helper function
+	additionalAttrs := getAttributesFromKeysAndValues(keysAndValues...)
+	attrs = append(attrs, additionalAttrs...)
+
+	// Create resource without SchemaURL to avoid community-reported conflicts
+	customResource, err := resource.New(
+		ctx,
+		resource.WithAttributes(attrs...),
 	)
-	return r
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := resource.Merge(
+		resource.Default(),
+		customResource,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
 }
 
 func newPropagator() propagation.TextMapPropagator {
@@ -81,7 +102,7 @@ func newPropagator() propagation.TextMapPropagator {
 	)
 }
 
-func newTraceProvider(ctx context.Context, serviceName, serviceVersion string, logger logr.Logger) (*tracesdk.TracerProvider, error) {
+func newTraceProvider(ctx context.Context, serviceName, serviceVersion string, logger logr.Logger, keysAndValues ...any) (*tracesdk.TracerProvider, error) {
 	logger.Info("Setting up OTel trace provider", "service", serviceName, "version", serviceVersion)
 	var traceProvider *tracesdk.TracerProvider
 
@@ -102,11 +123,17 @@ func newTraceProvider(ctx context.Context, serviceName, serviceVersion string, l
 			return nil, err
 		}
 
+		res, err := newResource(ctx, serviceName, serviceVersion, keysAndValues...)
+		if err != nil {
+			logger.Error(err, "failed to create resource")
+			return nil, err
+		}
+
 		traceProvider = tracesdk.NewTracerProvider(
 			tracesdk.WithBatcher(traceExporter,
 				// Default is 5s. Set to 1s for demonstrative purposes.
 				tracesdk.WithBatchTimeout(time.Second)),
-			tracesdk.WithResource(newResource(serviceName, serviceVersion)),
+			tracesdk.WithResource(res),
 		)
 	}
 
